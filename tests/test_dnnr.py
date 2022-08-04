295cf4cace3741fbd407ade506ec008234829762
Leon Sixt <github@leon-sixt.de>
2022-08-04 11:56:55 +0200

    Add a few test to test for bad data (e.g. constant) (#40)
    * Add a few test to test for bad data (e.g. constant)
    
    * Return cost = 0 and grad = 0 if targets match perfect

--- tests/test_dnnr.py
+++ tests/test_dnnr.py
@@ -3,6 +3,9 @@ import random
 from pathlib import Path
 
 import numpy as np
+import sklearn.datasets
+import sklearn.model_selection as sk_model_selection
+import sklearn.preprocessing as sk_preprocessing
 
 import dnnr
 
@@ -134,3 +137,73 @@ def test_dnnr_pickle(tmp_path: Path) -> None:
             loaded_model = pickle.load(fr)
 
         assert np.allclose(model.predict(x[:10]), loaded_model.predict(x[:10]))
+
+
+def test_binary_inputs():
+    np.random.seed(0)
+    x = np.random.normal(size=(500, 10))
+    w = 0.2 * np.random.normal(size=(10, 1)) + 2.0
+    # makes the last 5 dimensions unimportant
+    y = x @ w + (0.2 * x @ w) ** 2
+    y = y[:, 0]
+
+    x = (x > 0).astype(np.float32)
+
+    model = dnnr.DNNR(order='1', scaling='learned')
+    model.fit(x, y)
+    diff = np.abs(model.predict(x) - y).mean()
+    assert diff < 2.1
+
+
+def test_no_variability_in_target():
+    np.random.seed(0)
+    x = np.random.normal(size=(500, 10))
+    x = (x > 0).astype(np.float32)
+    y = np.ones(500)
+    model = dnnr.DNNR(order='1', scaling='learned')
+    model.fit(x, y)
+    diff = np.abs(model.predict(x) - y).mean()
+    assert diff < 0.1
+
+
+def test_no_variability_in_inputs():
+    np.random.seed(0)
+    x = np.ones((500, 10))
+    y = np.ones((500))
+    model = dnnr.DNNR(order='1', scaling='learned')
+    model.fit(x, y)
+    diff = np.abs(model.predict(x) - y).mean()
+    assert diff < 0.1
+
+
+def test_true_devide_california_housing():
+    np.random.seed(0)
+
+    cali = sklearn.datasets.fetch_california_housing()
+    std_scaler = sk_preprocessing.StandardScaler()
+
+    data = std_scaler.fit_transform(cali.data)
+    target = cali.target
+
+    # ------------------------------------------------------------
+
+    np.random.seed(0)
+    subset = np.random.choice(data.shape[0], size=1000, replace=False)
+    data = data[subset]
+    target = target[subset]
+
+    X_train, X_test, y_train, y_test = sk_model_selection.train_test_split(
+        data, target, test_size=0.15, random_state=0
+    )
+
+    model = dnnr.DNNR(
+        n_neighbors=3,
+        index='kd_tree',
+        scaling='learned',
+        scaling_kwargs=dict(n_epochs=2),
+        n_derivative_neighbors=5,
+    )
+
+    model.fit(X_train, y_train)
+
+    assert model.score(X_test[:100], y_test[:100]) < 0.7
